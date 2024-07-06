0ccee766d33137e945acb0ff286c19624b413986
Saveen Shrestha <biz.saveen@gmail.com>
2024-07-06 19:10:11 +0545

    added allauth package settings, extended FamilyMember model

--- tree/models.py
+++ tree/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.conf import settings
 
 
 class FamilyMember(models.Model):
@@ -9,16 +10,17 @@ class FamilyMember(models.Model):
         ('male', 'Male'),
         ('female', 'Female'),
     ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='family_members')
     name = models.CharField(max_length=100)
     birth_date = models.DateField(null=True, blank=True)
-    # death_date = models.DateField(null=True, blank=True)
+    death_date = models.DateField(null=True, blank=True)
     child_number = models.PositiveIntegerField(default=1)
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
     spouse = models.ForeignKey('self', null=True, blank=True, related_name='spouse_members', on_delete=models.CASCADE)
     sex = models.CharField(max_length=6, choices=SEX_CHOICES)
 
     def save(self, *args, **kwargs):
-        self.name = ' '.join(word.capitalize() for word in self.name.split())# Capitalize first letter of name
+        self.name = ' '.join(word.capitalize() for word in self.name.split()) # Capitalize first letter of name
         super().save(*args, **kwargs)
 
     def __str__(self):
